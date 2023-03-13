d457b627ac170f27fdf650232869ce597ca71c41
RCRecalde <rorecalde3@gmail.com>
2023-03-13 14:06:30 -0300

    estructura inicial completa

---
+++ README.md
@@ -0,0 +1,3 @@
+# REST API
+
+### CRUD de una REST API creada con Nodejs, babel, Express con autenticación (Json Web Token), autorización, roles(administradores y moderadores) y usando mongodb como base de datos.
---
+++ src/controllers/products.controller.js
@@ -0,0 +1,16 @@
+export const createProduct = (req,res) => {
+
+}
+export const getProducts = (req,res) => {
+    res.json('products')
+}
+
+export const getProductById = (req,res) => {
+    
+}
+export const updateProductById = (req,res) => {
+    
+}
+export const deleteProductById = (req,res) => {
+    
+}
