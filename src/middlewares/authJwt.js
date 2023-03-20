48d2017ef27b1bc5dff3087ba413746ceb6c7e63
RCRecalde <rorecalde3@gmail.com>
2023-03-20 17:18:01 -0300

    postman{

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -33,30 +33,36 @@ if(!user) return res.status(404).json({message: "User not found"})
 }
 
 export const isModerator = async (req, res, next) => {
-   const user = await User.findById(req.userId)
-   //busco los roles cuyos id´s esten en los roles del usuario
-   const roles = await Role.find({_id: {$in: user.roles}})
-   
-   for (let i = 0; i < roles.length; i++) {
-    if(roles[i].name == "moderator"){
-        next();
-        return;
-    }
-   }
-   return res.status(403).json({message: "Moderator role required"})
+    try {
+        const user = await User.findById(req.userId);
+        //busco los roles cuyos id´s esten en los roles del usuario
+        const roles = await Role.find({ _id: { $in: user.roles } });
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+        }
+        return res.status(403).json({ message: "Require Moderator Role" });
+      } catch (error) {
+        return res.status(500).send({ message: error });
+      }
 }
 export const isAdmin = async (req, res, next) => {
-    const user = await User.findById(req.userId)
-    const roles = await Role.find({_id: {$in: user.roles}})
+    try {
+        const user = await User.findById(req.userId);
+        const roles = await Role.find({ _id: { $in: user.roles } });
     
-    for (let i = 0; i < roles.length; i++) {
-     if(roles[i].name === "admin"){
-         next();
-         return;
-     }
-    }
- 
-    return res.status(403).json({
-     message: "Admin role required "
-    })
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "admin") {
+            next();
+            return;
+          }
+        }
+    
+        return res.status(403).json({ message: "Require Admin Role" });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: error });
+      }
 }
